51102361c4b48cb410f1c7ff3d2aed59b258f0af
Ali <ali_ovo@qq.com>
2022-04-22 22:52:07 +0800

    add heap

---
+++ heap/前 K 个高频元素.js
@@ -0,0 +1,88 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  swap(i1, i2) {
+    const temp = this.heap[i1];
+    this.heap[i1] = this.heap[i2];
+    this.heap[i2] = temp;
+  }
+
+  //  获取父节点
+  getParentIndex(index) {
+    // 除以二 取商
+    // return Math.floor((index - 1) / 2);
+    return (index - 1) >> 1;
+  }
+
+  getLeftIndex(i) {
+    return (i << 1) + 1;
+  }
+
+  getRightIndex(i) {
+    return (i << 1) + 2;
+  }
+
+  shiftUp(index) {
+    if (index == 0) return;
+    const parentIndex = this.getParentIndex(index);
+    if (this.heap[parentIndex] && this.heap[parentIndex].value > this.heap[index].value) {
+      this.swap(parentIndex, index);
+      this.shiftUp(parentIndex);
+    }
+  }
+
+  shiftDown(index) {
+    const leftIndex = this.getLeftIndex(index);
+    const rightIndex = this.getRightIndex(index);
+
+    if (this.heap[leftIndex] && this.heap[leftIndex].value < this.heap[index].value) {
+      this.swap(leftIndex, index);
+      this.shiftDown(leftIndex)
+    }
+
+    if (this.heap[rightIndex] && this.heap[rightIndex].value < this.heap[index].value) {
+      this.swap(rightIndex, index);
+      this.shiftDown(rightIndex)
+    }
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.shiftUp(this.heap.length - 1)
+  }
+
+  pop() {
+    this.heap[0] = this.heap.pop();
+    this.shiftDown(0);
+  }
+
+  peek() {
+    return this.heap[0]
+  }
+
+  size() {
+    return this.heap.length
+  }
+
+}
+
+
+// 时间复杂度 O(nlogk)
+// 空间复杂度 O(k)
+var topKFrequent = function (nums, k) {
+  const map = new Map();
+  nums.forEach(n => {
+    map.set(n, map.has(n) ? map.get(n) + 1 : 1);
+  })
+
+  const h = new MinHeap();
+  map.forEach((value, key) => {
+    h.insert({ value, key })
+    if (h.size() > k) {
+      h.pop()
+    }
+  })
+  return h.heap.map(item => item.key)
+};
